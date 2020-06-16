e1431699f51887cd46d85bc302d126e83d16fa31
renzy <renzy@fadada.com>
2020-06-16 10:15:52 +0800

    新建springboot 国际化练习demo

---
+++ springboot-i18n/src/main/java/com/test/i18n/international/InternationalApplication.java
@@ -0,0 +1,13 @@
+package com.test.i18n.international;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalApplication.class, args);
+    }
+
+}
